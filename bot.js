c081f0c32ccf23d0348f36c6ab5de82f47a348c8
devil43 <52785405+devil43@users.noreply.github.com>
2019-07-13 09:18:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ bot.on('message', message =>{
     // Variables
     var sender = message.author; //The person who sent the message
     var msg = message.content;
-    var prefix = ">" //The text before commands, you can put anything that you prefer
+    var prefix = "." //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "599436030914854942" && message.channel.id === "598841481423486979"){
-        if(msg.startsWith('>', 0)){
-            if(msg === prefix + "tf" && message.channel.id === "598841481423486979"){
-                message.channel.send('@here **आज से Server Private हो गया है।अतः सदस्यगणों से निवेदन है की Chat में Answer देने का प्रयास करे,जिसके परिणामस्वरूप,Text_Answer प्रदाता को उचित Role प्रदान की जाएगी।**') // Sends a message to the channel, with the content
+    if(message.author.id != "599436030914854942" && message.channel.id === "595630451301941249"){
+        if(msg.startsWith('.', 0)){
+            if(msg === prefix + "." && message.channel.id === "595630451301941249"){
+                message.channel.send('@here **आज से Server Private हो गया है।अतः सदस्यगणों से निवेदन है कि Chat में Answer देने का प्रयास करे,जिसके परिणामस्वरूप,Text_Answer प्रदाता को उचित Role प्रदान की जाएगी।**') // Sends a message to the channel, with the content
             }
             
         }
