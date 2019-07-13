ad547e4c78cc848a0cfc97d5f9c4c2180b5e2d42
devil43 <52785405+devil43@users.noreply.github.com>
2019-07-13 22:31:49 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,10 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "599436030914854942" && message.channel.id === "595630451301941249"){
+    if(message.author.id != "599436030914854942" && message.channel.id === "595630451301941249","598858800904142848","595821831999455232"){
         if(msg.startsWith('>', 0)){
-            if(msg === prefix + "tf" && message.channel.id === "595630451301941249"){
-                message.channel.send('@here **जो भी Member आज Text में Answer दिया है उन्हें Role मिल गयी है। अब देखने वाली बात है की कब तक Answer देता है और कब तक अपना Role बरक़रार रखता है।**') // Sends a message to the channel, with the content
+            if(msg === prefix + "tf" && message.channel.id === "595630451301941249","598858800904142848","595821831999455232"){
+                message.channel.send('@here **जो लोग Role मांग रहे है पहले Announcement पढ़े। **') // Sends a message to the channel, with the content
             }
             
         }
